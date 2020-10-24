e0efa80bee7aa64d8377e2a3fca6ce0b82530f5d
Emad Ghaffari <emadghaffariii@gmail.com>
2020-10-24 18:07:55 +0000

    add middlewares

--- app/application.go
+++ app/application.go
@@ -3,6 +3,7 @@ package app
 import (
 	"github.com/gin-gonic/gin"
 
+	"github.com/emadghaffari/grpc_rest_items_service/databases/elasticsearch"
 	"github.com/emadghaffari/res_errors/logger"
 )
 
@@ -12,7 +13,8 @@ var (
 
 // StartApplication func
 func StartApplication() {
+	elasticsearch.Init()
 	mapURL()
 	logger.Info("about to start application")
-	router.Run(":8080")
+	router.Run(":8000")
 }
--- app/mapurls.go
+++ app/mapurls.go
@@ -1,15 +1,22 @@
 package app
 
-import "github.com/emadghaffari/grpc_rest_items_service/controllers/ping"
-
-
-
-
-
+import (
+	"github.com/emadghaffari/grpc_rest_items_service/app/http/middleware"
+	"github.com/emadghaffari/grpc_rest_items_service/controllers/item"
+	"github.com/emadghaffari/grpc_rest_items_service/controllers/ping"
+)
 
 
 
 func mapURL()  {
 	// healthcheck for rest service
 	router.GET("/ping", ping.Ping)
+
+	authorized := router.Group("/items")
+	authorized.Use(middleware.AccessTokenMiddleware.CheckMiddleware)
+	authorized.POST("/",item.Create)
+	authorized.DELETE("/:id",item.Delete)
+
+	router.GET("/items/:id",item.Get)
+	router.POST("/items/search",item.Search)
 }
